413951ac37203bfb69e14769a5b2c3f7bfe9fd0f
zhangmingliang0714 <zhang.mingliang@icntv.tv>
2015-10-19 11:28:49 +0800

    V1.1.1 --> V1.1.2

--- jni/common.h
+++ jni/common.h
@@ -1,10 +1,10 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
-#define GIT_VERSION "20150924-04243e4"
+#define GIT_VERSION "20151009-fabfc98"
 
 #if (SDK_TYPE == 1)  //ANDROID_JAR
-    #define SDK_VERSION "1.1.1"
+    #define SDK_VERSION "1.1.2"
 #elif (SDK_TYPE == 2)  //ANDROID_PURESDK
     #define SDK_VERSION "1.0.0"
 #else  //others
