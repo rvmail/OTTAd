fb79844c582d067c8785575310b344d891edf9ad
zhangmingliang0714 <zhang.mingliang@icntv.tv>
2016-01-28 11:45:56 +0800

    update common.h

--- jni/common.h
+++ jni/common.h
@@ -1,7 +1,7 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
-#define GIT_VERSION "20160127-f2439c3"
+#define GIT_VERSION "20160128-fed0e80"
 
 #if (SDK_TYPE == 1)  //ANDROID_JAR
     #define SDK_VERSION "V1.2.3"
